85cc66e25723d42c3a9612e1c462599d0bdd2e36
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-04-29 11:47:12 -0400

    Java Programming Day02

---
+++ src/day02_escapeSequences/EmployeeInfo.java
@@ -0,0 +1,18 @@
+package day02_escapeSequences;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name:  Daniel");
+        System.out.println("Gender:  Male");
+        System.out.println("Age:  25");
+        System.out.println("Job title:  Java Developer");
+        System.out.println("Company:  Apple Inc");
+        System.out.println("Salary:  90000");
+
+
+    }
+
+
+}
