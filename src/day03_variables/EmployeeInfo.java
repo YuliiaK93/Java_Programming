4f8406948b531e3d1004f5a9416ca424c6f459a9
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 15:00:11 -0400

    Java Programming Day03

--- src/day03_variables/EmployeeInfo.java
+++ src/day03_variables/EmployeeInfo.java
@@ -4,8 +4,6 @@ public class EmployeeInfo {
 
     public static void main(String[] args) {
 
-        // employee name is Aaron
-
         /*
 
         System.out.println("Aaron");
@@ -27,6 +25,56 @@ public class EmployeeInfo {
 
 
 
+        String name = "Emily";
+        int age = 30;
+        char gender = 'F';
+        String companyName = "Google Inc";
+        String employeeId = "D11";
+        String jobTitle = "Scrum Master";
+        int salary = 110000;
+        boolean isFullTime = false;
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(companyName);
+        System.out.println(employeeId);
+        System.out.println(jobTitle);
+        System.out.println(salary);
+        System.out.println(isFullTime);
+
+        System.out.println("-----------------------------------------------------");
+
+
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("companyName = " + companyName);
+        System.out.println("employeeId = " + employeeId);
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("salary = " + salary);
+        System.out.println("isFullTime = " + isFullTime);
+
+
+
+
+
+
+
     }
 
 }
+
+/*
+Declare the following variables with appropriate data types:
+name
+age
+gender
+companyName
+employeeId
+jobTitle
+salary
+isFullTime
+
+ */
