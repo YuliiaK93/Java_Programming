b9c238aae2561bd55c90c212feb951ad4198f990
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 15:33:14 -0400

    Java Programming Day03

---
+++ src/day03_variables/ReplaceVariableName.java
@@ -0,0 +1,26 @@
+package day03_variables;
+
+public class ReplaceVariableName {
+
+    public static void main(String[] args) {
+        
+        String student_name = "Josh";
+
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+
+        double salary = 100000.5;
+
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+
+        
+    }
+    
+}
