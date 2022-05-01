fbfce7deeea77e7c3a2f697cdacd2662fcb80e19
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 14:36:43 -0400

    Java Programming Day03

--- src/day03_variables/VariableNamingRules.java
+++ src/day03_variables/VariableNamingRules.java
@@ -18,6 +18,22 @@ public class VariableNamingRules {
 
         double  salary_after_tax$  = 80000.5;
 
+        System.out.println("-----------------------------------");
+
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+
+
+      //  double abstract = 5.5;  // variables names can not be java reserved words
+      //  double final = 85.5;
+
+        System.out.println("-----------------------------------");
+
+        // gender is male
+        String gender = "Male";
+
+        String fullName = "Cydeo School";
 
 
 
