b96bed4a79ce25609c5395ca3e290ab07c2ed6e1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 15:38:45 -0400

    Java Programming Day03

---
+++ src/day03_variables/StudentInfo.java
@@ -0,0 +1,16 @@
+package day03_variables;
+
+public class StudentInfo {
+}
+
+/*
+Declare the following variables with appropriate data types:
+    students_name
+    age
+    gender
+    school_name
+    student_id
+    grade_level
+    gpa
+
+ */
