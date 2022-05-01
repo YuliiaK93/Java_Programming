3fb2bdcf3036eec39a4e017368be1e4d560874ff
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 11:55:56 -0400

    Java Programming Day03

---
+++ src/day03_variables/PrimitiveDataTypesIntro.java
@@ -0,0 +1,31 @@
+package day03_variables;
+
+public class PrimitiveDataTypesIntro {
+
+    public static void main(String[] args) {
+
+        // DataType variableName = Data
+
+       // byte a = "Java"; //byte only accepts integer numbers (-128 ~127)
+      //  byte a = 20.5;  // byte can not take decimal number
+     //   byte a = 2000; // out of Byte' range  (-128 ~127)
+        byte a = 20;
+
+    // price of the car is $17500
+
+        short p = 17500;
+
+    // salary is 95000
+        int s = 95000;
+
+     //   int number = 9999999999;
+        long number = 9999999999L;
+
+
+
+
+
+
+    }
+
+}
