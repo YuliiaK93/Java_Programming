7cd21bbfc7e24ad4c53d985155631550f6834887
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 14:41:41 -0400

    Java Programming Day03

---
+++ src/day03_variables/EmployeeInfo.java
@@ -0,0 +1,32 @@
+package day03_variables;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        // employee name is Aaron
+
+        /*
+
+        System.out.println("Aaron");
+
+        System.out.println("Aaron");
+
+        System.out.println("Aaron");
+
+        System.out.println("--------------------------------");
+
+        String name = "Aaron";
+
+        System.out.println(name);
+
+        System.out.println(name);
+
+        System.out.println(name);
+        */
+
+
+
+    }
+
+}
