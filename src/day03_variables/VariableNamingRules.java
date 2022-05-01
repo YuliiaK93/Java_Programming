da4f62bb7f4f015aa4470be727198bd2d2a97ca1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-01 13:08:11 -0400

    Java Programming Day03

---
+++ src/day03_variables/VariableNamingRules.java
@@ -0,0 +1,28 @@
+package day03_variables;
+
+public class VariableNamingRules {
+
+    public static void main(String[] args) {
+
+        // age of the student is 28
+        // salary is 10000
+
+        int age = 28;
+        int salary = 10000;
+
+        // int age = 32; // Variable name MUST be UNIQUE
+
+        int phoneNumber = 123456789;  // camel case
+
+        double salaryBeforeTax = 100000.5;
+
+        double  salary_after_tax$  = 80000.5;
+
+
+
+
+
+    }
+
+
+}
