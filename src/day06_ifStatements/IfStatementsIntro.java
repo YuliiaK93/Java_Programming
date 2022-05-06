5c87afc8051e391306d3347989249929e2f785d0
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-06 14:55:21 -0400

    Java Programming Day06: If Statements

---
+++ src/day06_ifStatements/IfStatementsIntro.java
@@ -0,0 +1,27 @@
+package day06_ifStatements;
+
+public class IfStatementsIntro {
+
+    public static void main(String[] args) {
+
+        int number = 200;
+
+        boolean isPositive = number > 0 ;
+
+        if(isPositive){
+            System.out.println(number + " is positive number");
+        }
+
+
+        System.out.println(number + " is negative number");
+
+
+        System.out.println(number + " is zero");
+
+
+
+
+
+    }
+
+}
