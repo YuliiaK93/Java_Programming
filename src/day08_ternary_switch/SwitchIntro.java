55b02e9ef61d4e97b762c4f6dc663bb8a2b05a37
Muhtar <muhtar@cybertekschool.comn>
2022-05-10 12:43:42 -0700

    Java Programming Day08

--- src/day08_ternary_switch/SwitchIntro.java
+++ src/day08_ternary_switch/SwitchIntro.java
@@ -4,8 +4,7 @@ public class SwitchIntro {
 
     public static void main(String[] args) {
 
-        char grade = 'A';
-
+        char grade = 'Q';
 
         /*
         A: Excellent
@@ -15,8 +14,51 @@ public class SwitchIntro {
         F: Failed
          */
 
+        /*
+        String result;
 
+        if (grade == 'A'){
+            result = "Excellent";
+        }else if (grade == 'B'){
+            result = "Great Job";
+        }else if(grade == 'C'){
+            result = "Good";
+        }else if(grade == 'D'){
+            result = "Passed";
+        }else {
+            result = "Failed";
+        }
 
+        System.out.println(result);
+
+        System.out.println("----------------------------------");
+*/
+        switch (grade){
+
+            case 'B':
+                System.out.println("Great Job");
+                break;
+
+            case 'A':
+                System.out.println("Excellent");
+                break;
+
+            case 'C':
+                System.out.println("Good");
+                break;
+
+            case 'D':
+                System.out.println("Passed");
+                break;
+
+            case 'F':
+                System.out.println("Failed");
+                break;
+
+            default:
+                System.out.println("Invalid");
+
+        }
 
 
 
