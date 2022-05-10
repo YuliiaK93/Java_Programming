c36109c3854f089f36967d5a4c04198376235c99
Muhtar <muhtar@cybertekschool.comn>
2022-05-10 11:40:39 -0700

    Java Programming Day08

--- src/day08_ternary_switch/TernaryIntro3.java
+++ src/day08_ternary_switch/TernaryIntro3.java
@@ -22,26 +22,23 @@ public class TernaryIntro3 {
         System.out.println(result);
 */
 
-        String result = (score >= 0 && score <= 100)?
-                            (score >= 60) ?  "Passed" : "Failed"
-                        : "Invalid Score" ;
+        String result = (score >= 0 && score <= 100) ?
+                (score >= 60) ? "Passed" : "Failed"
+                : "Invalid Score";
 
         System.out.println(result);
 
+        System.out.println("----------------------------------------");
+
+        int n = 9;
+
+        String day = (n >= 1 && n <= 7) ?
+                        (n == 1) ? "Monday" : (n == 2) ? "Tuesday" : (n == 3) ? "Wednesday" : (n == 4) ? "Thursday"
+                                : (n == 5) ? "Friday" : (n == 6) ? "Saturday" : "Sunday"
+                : "No such a day";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println(day);
 
     }
 
