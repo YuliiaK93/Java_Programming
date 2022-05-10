93e516d7af6db9c20a65825fab42baba3d4a0bc9
Muhtar <muhtar@cybertekschool.comn>
2022-05-10 12:03:40 -0700

    Java Programming Day08

---
+++ src/day08_ternary_switch/SwitchIntro.java
@@ -0,0 +1,25 @@
+package day08_ternary_switch;
+
+public class SwitchIntro {
+
+    public static void main(String[] args) {
+
+        char grade = 'A';
+
+
+        /*
+        A: Excellent
+        B: Great Job
+        C: Good
+        D: Passed
+        F: Failed
+         */
+
+
+
+
+
+
+    }
+
+}
