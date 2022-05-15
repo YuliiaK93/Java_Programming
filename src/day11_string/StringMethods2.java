b1cf13b7d4e6243bd4518c790af6d9c16a91fbc7
Muhtar <muhtar@cybertekschool.comn>
2022-05-15 12:31:21 -0700

    Java Programming Day11

--- src/day11_string/StringMethods2.java
+++ src/day11_string/StringMethods2.java
@@ -40,6 +40,57 @@ public class StringMethods2 {
         System.out.println(sentence3);
 
 
+        //----------------------------------------------------------
+
+        String sentence4 = "I love Java Programming";
+
+        String languageName = sentence4.substring(7, 10 + 1);
+
+        System.out.println(languageName);
+
+        //----------------------------------------
+
+        String sentence5 = "Today is Sunday, Tomorrow is Monday";
+        //                  0123456789
+
+        String today = sentence5.substring(9, 14+1);
+
+        System.out.println(today);
+
+        //----------------------------------------
+
+        String email = "CydeoSchoolJavaProgramming@yahoo.com";
+
+        int beginning = email.indexOf("@") + 1;
+        int ending = email.lastIndexOf('.');
+
+        String domain =  email.substring(beginning, ending);
+
+        System.out.println(domain);
+
+        //----------------------------------------
+
+        String sentence6 = "I love Java programming";
+
+       // String r1 = sentence6.substring(7, sentence6.length());
+
+        String r1 = sentence6.substring(7);
+
+        System.out.println(r1);
+
+
+        System.out.println("------------------------------------");
+
+        String sentence7 = "Today is Sunday, Tomorrow is Monday";
+
+        String tomorrow =  sentence7.substring( sentence7.lastIndexOf(" ") + 1 );
+        // create the substring "Monday" from sentence7. DO NOT count any index numbers
+
+        System.out.println(tomorrow);
+
+
+
+
 
     }
 
