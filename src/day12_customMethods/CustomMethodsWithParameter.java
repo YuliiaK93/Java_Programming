863d74f83031785b3e99d17a55861148fbf563d1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-17 15:10:01 -0400

    Java Programming Day12

---
+++ src/day12_customMethods/CustomMethodsWithParameter.java
@@ -0,0 +1,34 @@
+package day12_customMethods;
+
+public class CustomMethodsWithParameter {
+
+    public static void main(String[] args) {
+
+        oddOrEven(100);
+
+        System.out.println("--------------------------");
+
+        oddOrEven(10000);
+
+    }
+
+
+    // the method takes an argument number, and verifies if the number is odd number or even number
+    public static void oddOrEven(int number){
+
+        if(number % 2 ==0){
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number+" is odd number");
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
