a9987e29cb295bcaf905233680e3cc2ce5bac718
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-17 11:34:23 -0400

    Java Programming Day12

--- src/day12_customMethods/FormatFullName.java
+++ src/day12_customMethods/FormatFullName.java
@@ -4,8 +4,19 @@ public class FormatFullName {
 
     public static void main(String[] args) {
 
-        String first = "cyDEo";
-        String last =  "SCHOOL";
+        String first = "tAtiaNA";
+        String last = "seREMEt";
+
+
+        first = first.substring(0, 1).toUpperCase() + first.substring(1).toLowerCase();
+        //getting first character of first name  +  the remaining characters of the first name
+
+        last = last.substring(0, 1).toUpperCase() + last.substring(1).toLowerCase();
+
+        String full_name = first + " " + last;
+
+        System.out.println(full_name);
+
 
 
     }
