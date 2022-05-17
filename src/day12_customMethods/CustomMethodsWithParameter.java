cfa657e9db660a0b3d435af67879547d362dce79
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-17 15:12:58 -0400

    Java Programming Day12

--- src/day12_customMethods/CustomMethodsWithParameter.java
+++ src/day12_customMethods/CustomMethodsWithParameter.java
@@ -26,6 +26,16 @@ public class CustomMethodsWithParameter {
 
 
 
+    // create a method that can check if a person is eligible to buy alcohol  (age)
+
+
+
+    // create a method that can check if a number is odd, or even or zero (number)
+
+
+
+    // create a method that can calculate the grade of the students  (score)
+
 
 
 
