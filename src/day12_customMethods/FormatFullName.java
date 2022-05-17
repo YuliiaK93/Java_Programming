187f1cb10f18d9b02e19c860f0c85bb4833eb7f1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-17 11:10:19 -0400

    Java Programming Day12

---
+++ src/day12_customMethods/FormatFullName.java
@@ -0,0 +1,25 @@
+package day12_customMethods;
+
+public class FormatFullName {
+
+    public static void main(String[] args) {
+
+        String first = "cyDEo";
+        String last =  "SCHOOL";
+
+
+    }
+
+}
+
+/*
+1. Write a program that asks user to enter first and last names, and then prints the full name in regular format (first character in upper case)
+
+	                    input:
+	                        firstName = "cyDEo"
+	                        lastName = "SCHOOL";
+
+	                    output:
+	                        Cydeo School
+
+ */
