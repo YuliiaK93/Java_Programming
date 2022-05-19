b40f29276ac2cd1ca91854dda9679d51deb3008c
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-19 13:12:26 -0400

    Java Programming Day13

---
+++ src/day13_customMethods/ReturnMethodsPractice.java
@@ -0,0 +1,17 @@
+package day13_customMethods;
+
+public class ReturnMethodsPractice {
+
+
+}
+
+/*
+Create a method named isOdd, that can return true if a number is an odd number
+
+Create a method named isEven, that can return true if a number is an even number
+
+Create a method named max, that can return the maximum number between two numbers
+
+Create a method named min, that can return the minimum number between two numbers
+
+ */
