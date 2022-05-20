6ef179b36e3db38ad6323a25300e40b0614f0a9d
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-20 11:40:33 -0400

    Java Programming Day14

--- src/day14_forLoop/WarmupTasks.java
+++ src/day14_forLoop/WarmupTasks.java
@@ -9,18 +9,37 @@ public class WarmupTasks {
         System.out.println(str3);
 
 
+      String s1 = "" + true; //"true"
+
+        String s2 = ""+'e'; // "e"
+
+        System.out.println("-----------------------------------------------");
+
+
+
+
+
     }
 
-                                //one,         eight
+
     public static String combine(String str1, String str2){
 
         String result;
 
-        if( str1.endsWith( ""+str2.charAt(0) )   ){  // if the first string ends with the first character of the second string
+        /*
+        if( str1.endsWith(  ""+str2.charAt(0) )   ){  // if the first string ends with the first character of the second string
             result = str1 + str2.substring(1);
         }else{
             result = str1 + str2;
         }
+         */
+
+        if( str1.charAt(str1.length()-1) == str2.charAt(0)  ){ // if the last character in first string is equal to the first character of the second string
+            result =  str1 + str2.substring(1); // first character of the second string should be excluded in the concatenation
+        }else{
+            result = str1 + str2;
+        }
+
 
         return result;
     }
