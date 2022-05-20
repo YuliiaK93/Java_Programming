e385102bbcd860e59f737ba3580bd8888c6b156b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-20 16:16:34 -0400

    Java Programming Day14

---
+++ src/day14_forLoop/ForLoopPractice.java
@@ -0,0 +1,27 @@
+package day14_forLoop;
+
+public class ForLoopPractice {
+
+    public static void main(String[] args) {
+
+        for( int i = 10;  i >= 5; i--){ // i: 10, 9, 8, 7, 6, 5, 4
+            System.out.println("Hello Cydeo "+ i); // 1, 2, 3, 4, 5, 6
+        }
+
+
+      //  System.out.println("Hello World");
+
+        System.out.println("------------------------------------------");
+
+        // sum of all the numbers 1 ~ 100
+
+        // print all the alphabet letters A ~Z
+
+        // print all the alphabet letters in backwards
+
+
+
+    }
+
+
+}
