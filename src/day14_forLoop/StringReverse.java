6e22e2022d2253642247fa0a21427eb38cab98b7
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-20 17:22:19 -0400

    Java Programming Day14

--- src/day14_forLoop/StringReverse.java
+++ src/day14_forLoop/StringReverse.java
@@ -12,8 +12,8 @@ public class StringReverse {
 
         String reverse = ""; //"avaJ"
 
-        for(int i = str.length() - 1 ; i >= 0; i-- ){
-           reverse += str.charAt(i);
+        for(int i = str.length() - 1 ; i >= 0; i-- ){ // i: index number of the given string starting from last index to index 0
+            reverse += str.charAt(i);  // to get each character of the string starting from last index to index zero
         }
 
 
