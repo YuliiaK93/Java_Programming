85c675b0b7c70e3f1975b43a589277597b564f7f
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-22 14:49:20 -0400

    Java Programming Day15

--- src/day15_whileLoop/ContinueStatement.java
+++ src/day15_whileLoop/ContinueStatement.java
@@ -38,16 +38,44 @@ public class ContinueStatement {
 
         }
 
+        System.out.println("--------------------------------------------");
+
+        // find the sum of all the even number between 50 ~ 100
+
+        for (int i = 50; i < 101; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i +" ");
+            }
+        }
+
+        System.out.println();
+
+
+        //50, 52, 54, 56 ...... 100
+        for (int i = 50; i < 101; i += 2) {
+            System.out.print(i+" ");
+        }
+
+        System.out.println();
+
+
+        for (int i = 50; i < 101; i++) {
+            if (i % 2 != 0){ // if the i's value is odd number
+                continue; // skip it
+            }
+
+            System.out.print(i+" ");
+
+        }
+
+        System.out.println();
+
 
 /*
         for (int i = 0; i < 10; i++)
             System.out.println("Hello World");
 
             */
-
-
-
-
 
 
     }
