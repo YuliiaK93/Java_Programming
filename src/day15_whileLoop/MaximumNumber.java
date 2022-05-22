52e584eb8d74d9f94236553cb0c47a1e8501427e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-22 11:07:10 -0400

    Java Programming Day15

---
+++ src/day15_whileLoop/MaximumNumber.java
@@ -0,0 +1,15 @@
+package day15_whileLoop;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+/*
+1. Write a program that can ask the user to enter a number for 5 times and returns the maximum number
+ */
