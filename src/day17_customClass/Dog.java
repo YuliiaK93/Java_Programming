db8ae0c9e01bbb8b704a71f5d034a55459b0f88e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-29 13:09:20 -0400

    Java Programming Day17

---
+++ src/day17_customClass/Dog.java
@@ -0,0 +1,21 @@
+package day17_customClass;
+
+public class Dog {
+
+    public String name;  // instance variable
+    public String breed;
+    public String size;
+    public int age;
+    public String color;
+
+
+    public void eat(){
+        System.out.println(name +" is eating");
+    }
+
+    public void drink(){
+        System.out.println(name +" is drinking water");
+    }
+
+
+}
