02f6a6ec463569acde5dbb70f8cc55c5aa62615f
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-05-31 12:09:11 -0400

    Java Programming Day18

--- src/day18_garbageCollection/TestPizzaObjects.java
+++ src/day18_garbageCollection/TestPizzaObjects.java
@@ -21,6 +21,29 @@ public class TestPizzaObjects {
         System.out.println( pizza1 );
         System.out.println(pizza2);
 
+        System.out.println("-------------------------------------------------");
+
+        double total = 0;
+
+        for (int i = 0; i < 20; i++) {
+
+            Pizza small = new Pizza();
+            small.setInfo('S', 2, 2);
+            total += small.calcCost();
+
+            Pizza medium = new Pizza();
+            medium.setInfo('M', 3, 4);
+            total += medium.calcCost();
+
+            Pizza large = new Pizza();
+            large.setInfo('L', 4, 5);
+            total += large.calcCost();
+
+        }
+
+
+        System.out.println("total = " + total);
+
 
 
 
