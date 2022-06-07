9b01f0df4fcb8979d98f4c836186cd5152166a96
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-07 12:41:20 -0400

    Java Programming Day20

--- src/day20_forEach/Reverse.java
+++ src/day20_forEach/Reverse.java
@@ -1,5 +1,7 @@
 package day20_forEach;
 
+import utilities.ArraysUtility;
+
 import java.util.Arrays;
 
 public class Reverse {
@@ -17,6 +19,21 @@ public class Reverse {
 
         System.out.println(Arrays.toString(reverse));
 
+        System.out.println("------------------------------------------");
+
+        int[] nums = {100, 200, 300, 400, 500};
+        nums = ArraysUtility.reverse(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+
+        System.out.println("------------------------------------------");
+
+        double[] a1 = {1.5, 2.5, 3.5, 4.5};
+
+        a1 = ArraysUtility.reverse(a1);
+
+        System.out.println(Arrays.toString(a1));
 
 
     }
