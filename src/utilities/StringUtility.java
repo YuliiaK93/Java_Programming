57b9e54bde92a818c21931c1f5b0d85d6b313f77
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-07 12:11:42 -0400

    Java Programming Day20

--- src/utilities/StringUtility.java
+++ src/utilities/StringUtility.java
@@ -14,6 +14,54 @@ public class StringUtility {
     }
 
 
+    // return the frequency of the given word from the given sentence
+    public static int frequency(String sentence, String word){
+
+        int count = 0;
+
+        while(sentence.contains(word)){
+            sentence = sentence.replaceFirst(word, "");
+            count++;
+        }
+
+        return count;
+    }
+
+
+    //                          "aabacde",    'a'
+    public static int frequency(String str, char ch){
+        int count =0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ch){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+
+
+
+
+    public static String removeDup(String str){
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);
+
+            if(!result.contains(""+each)) {
+                result += each;
+            }
+
+        }
+
+        return result;
+    }
+
+
+
 
 
 
