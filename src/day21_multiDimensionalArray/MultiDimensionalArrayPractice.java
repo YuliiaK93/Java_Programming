9bfe65fa17843e79924cdc59d5fbaa0e75812d70
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-09 14:22:07 -0400

    Java Programming Day21

--- src/day21_multiDimensionalArray/MultiDimensionalArrayPractice.java
+++ src/day21_multiDimensionalArray/MultiDimensionalArrayPractice.java
@@ -1,17 +1,40 @@
 package day21_multiDimensionalArray;
 
+import java.util.Arrays;
+
 public class MultiDimensionalArrayPractice {
 
     public static void main(String[] args) {
 
-        String[] group1 = {"James", "Daniel"};  // 4 names
-        String[] group2 = {};  // 3 names
-        String[] group3 = {};  // 5 names
-        String[] group4 = {};  // 2 names
+        String[] group1 = {"Lucy", "Umran", "Sumeye", "Abidullah"}; //4names;
+        String[] group2 = {"Madiyar", "Khashayar", "Rayhane"}; //3 names;
+        String[] group3 = {"Alena", "Evgeniya", "Shukur", "Hasan", "Bilal"}; //5 names;
+        String[] group4 = {"Adelina", "Ali"}; //2 names;
 
 
-        String[][] groups =  {group1, group2, group3, group4};
+        String[][] groups = {group1, group2, group3, group4};
+        //                      0        1       2       3
 
+        for (int i = 0; i < groups.length; i++) {
+            String[] eachGroup = groups[i];
+            System.out.println(Arrays.toString(eachGroup));
+
+            for (int j = 0; j < eachGroup.length; j++) {
+                String eachStudent = eachGroup[j];
+                System.out.println(eachStudent);
+            }
+
+        }
+
+
+        System.out.println("-----------------------------------------------------------");
+
+        for (String[] eachGroup : groups) {
+            System.out.println(Arrays.toString(eachGroup) );
+            for (String eachStudent : eachGroup) {
+                System.out.println(eachStudent);
+            }
+        }
 
 
 
