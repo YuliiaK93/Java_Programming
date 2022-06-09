87665df05fa6bbd0e066eebdc642cf476cb0f79d
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-09 13:09:31 -0400

    Java Programming Day21

---
+++ src/day21_multiDimensionalArray/MultiDimensionalArrayPractice.java
@@ -0,0 +1,21 @@
+package day21_multiDimensionalArray;
+
+public class MultiDimensionalArrayPractice {
+
+    public static void main(String[] args) {
+
+        String[] group1 = {"James", "Daniel"};  // 4 names
+        String[] group2 = {};  // 3 names
+        String[] group3 = {};  // 5 names
+        String[] group4 = {};  // 2 names
+
+
+        String[][] groups =  {group1, group2, group3, group4};
+
+
+
+
+    }
+
+
+}
