26128914715ca45fd4d4e113b5154bcaa4171f6e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-12 12:38:41 -0400

    Java Programming Day23

--- src/day23_arrayList/ArrayListMethods.java
+++ src/day23_arrayList/ArrayListMethods.java
@@ -41,6 +41,48 @@ public class ArrayListMethods {
         System.out.println(groceriesList);
 
 
+        System.out.println("-------------------------------------------------");
+
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10); // 0
+        numbers.add(20); // 1
+        numbers.add(30); // 2
+        numbers.add(40); // 3
+        numbers.add(50); // 4
+
+
+        boolean r1 = numbers.remove( Integer.valueOf(10) );
+
+        System.out.println(numbers);
+
+        System.out.println(r1);
+
+        numbers.clear();
+
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+
+
+        System.out.println("-------------------------------------------------");
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Vasyl"); // 0
+        names.add("Vasyl"); // 1
+        names.add("Sumeye"); //2
+        names.add("Sumeye");
+        names.add("Ali");
+        names.add("Sumeye");
+
+        System.out.println(  names.indexOf("Vasyl") );
+        System.out.println( names.lastIndexOf("Vasyl") );
+
+        System.out.println( names.lastIndexOf("Sumeye") );
+
+
+
+
 
 
     }
