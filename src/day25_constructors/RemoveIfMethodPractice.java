d2804d2faedc93e58ed04c41e30cce428c4d8906
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-17 11:40:57 -0400

    Java Programming Day25

--- src/day25_constructors/RemoveIfMethodPractice.java
+++ src/day25_constructors/RemoveIfMethodPractice.java
@@ -34,13 +34,48 @@ public class RemoveIfMethodPractice {
         System.out.println(smallDogs);
 
 
-        ArrayList<Dog> youngDogs = new ArrayList<>();
+        ArrayList<Dog> youngDogs = new ArrayList<>( Arrays.asList(dogs) );
 
+        youngDogs.removeIf( p -> p.age > 4);
+
+        /*
         for (Dog dog : dogs) {
             if(dog.age <= 4){
                 youngDogs.add(dog);
             }
         }
+
+         */
+
+        System.out.println(youngDogs);
+
+       // youngDogs.forEach( p -> System.out.println(p.name) );
+
+
+        ArrayList<Dog> femaleDogs = new ArrayList<>(Arrays.asList(dogs));
+        femaleDogs.removeIf(p -> p.gender == 'M');
+
+        ArrayList<Dog> maleDogs = new ArrayList<>(Arrays.asList(dogs));
+        maleDogs.removeIf( p -> p.gender == 'F');
+
+
+        System.out.println("----------------------------------------------------------------------");
+
+        Dog[] dogs2 = {new Dog(), new Dog(), new Dog(), new Dog(), new Dog(), new Dog()};
+        dogs2[0].setInfo("Max", "Husky", 'M', 4, "Medium", "Gray");
+        dogs2[1].setInfo("Chloe", "Golden Retriever", 'F', 8, "Large", "Gold");
+        dogs2[2].setInfo("Molly", "Beagle", 'F', 5, "Small", "White");
+        dogs2[3].setInfo("Oliver", "Bulldog", 'M', 2, "Small", "Black");
+        dogs2[4].setInfo("Bella", "Bichon Frise", 'F', 3, "Small", "White");
+        dogs2[5].setInfo("Loki", "Chow Chow", 'M', 7, "Medium", "Gold");
+
+
+        ArrayList<Dog> list = new ArrayList<>( Arrays.asList(dogs2) );
+        list.removeIf(p -> p.size.equalsIgnoreCase("small"));
+
+        dogs2 = list.toArray(new Dog[list.size()]);
+
+        System.out.println(Arrays.toString(dogs2));
 
 
 
