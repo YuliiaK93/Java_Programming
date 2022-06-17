2c4bcbf6ea12cb4712e2681e137baf7ae83bd409
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-17 16:42:44 -0400

    Java Programming Day25

--- src/day25_constructors/Rectangle.java
+++ src/day25_constructors/Rectangle.java
@@ -4,10 +4,19 @@ public class Rectangle {
 
     public double length, width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    /*
     public void setInfo(double length, double width){
         this.length = length;
         this.width = width;
     }
+
+     */
+
 
     public double area(){
         return length * width;
@@ -17,7 +26,9 @@ public class Rectangle {
         return "Rectangle{" +
                 "length=" + length +
                 ", width=" + width +
+                ", area=" + area() +
                 '}';
     }
+
 
 }
