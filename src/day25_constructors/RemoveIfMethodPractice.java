d45913b2319e8ed3ccd91c62879e0e1d9cf8d694
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-17 11:24:55 -0400

    Java Programming Day25

--- src/day25_constructors/RemoveIfMethodPractice.java
+++ src/day25_constructors/RemoveIfMethodPractice.java
@@ -1,12 +1,57 @@
-Java Programming Day25
+package day25_constructors;
 
-Topic: Date & Time
-	   Constructors
+import day17_customClass.Dog;
 
-package name: day25_constructor
+import java.util.ArrayList;
+import java.util.Arrays;
 
-warmup tasks:
-	1. Give the following array of dogs
+public class RemoveIfMethodPractice {
+
+    public static void main(String[] args) {
+
+        Dog[] dogs = {new Dog(), new Dog(), new Dog(), new Dog(), new Dog(), new Dog()};
+        dogs[0].setInfo("Max", "Husky", 'M', 4, "Medium", "Gray");
+        dogs[1].setInfo("Chloe", "Golden Retriever", 'F', 8, "Large", "Gold");
+        dogs[2].setInfo("Molly", "Beagle", 'F', 5, "Small", "White");
+        dogs[3].setInfo("Oliver", "Bulldog", 'M', 2, "Small", "Black");
+        dogs[4].setInfo("Bella", "Bichon Frise", 'F', 3, "Small", "White");
+        dogs[5].setInfo("Loki", "Chow Chow", 'M', 7, "Medium", "Gold");
+
+
+        ArrayList<Dog> smallDogs = new ArrayList<>( Arrays.asList(dogs) );
+
+        smallDogs.removeIf( p -> !p.size.equalsIgnoreCase("Small"));
+
+
+        /*
+        for (Dog dog : dogs) {
+            if(dog.size.equalsIgnoreCase("Small")){
+                smallDogs.add(dog);
+            }
+        }
+*/
+
+        System.out.println(smallDogs);
+
+
+        ArrayList<Dog> youngDogs = new ArrayList<>();
+
+        for (Dog dog : dogs) {
+            if(dog.age <= 4){
+                youngDogs.add(dog);
+            }
+        }
+
+
+
+
+
+    }
+
+}
+
+/*
+1. Give the following array of dogs
             Dog[] dogs = {new Dog(), new Dog(), new Dog(), new Dog(), new Dog(), new Dog()};
             dogs[0].setInfo("Max", "Husky", 'M', 4, "Medium", "Gray");
             dogs[1].setInfo("Chloe", "Golden Retriever", 'F', 8, "Large", "Gold");
@@ -16,7 +61,7 @@ warmup tasks:
             dogs[5].setInfo("Loki", "Chow Chow", 'M', 7, "Medium", "Gold");
 
             Note: Dog class is imported from package day17
-            
+
         1.1 Create an arrayList named smallDogs, and add all the dog objects with the size of small from the dogs array to smallDogs arrayList
 
             Note: DO NOT use any loops
@@ -32,9 +77,4 @@ warmup tasks:
 	    1.4 Create an arrayList named maleDogs, and add all the dog objects with the gender of 'M' the dogs array to maleDogs arrayList
 
 	            Note: DO NOT use any loops
-
-
-
-
-
-
+ */
