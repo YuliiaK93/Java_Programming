a1b045fd77484a1de6ab1fbf617b0d696b2e8211
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-17 17:10:22 -0400

    Java Programming Day25

--- src/day25_constructors/Person.java
+++ src/day25_constructors/Person.java
@@ -7,4 +7,8 @@ public class Person {
 
 /*
 name, age, gender, dateOfBirth, isMarried, isEmployed
+
+Add a constructor that can set all the fields once an object is created
+
+toString(), eat(String food), sleeping(), drink(String drink)
  */
