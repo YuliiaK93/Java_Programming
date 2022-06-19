aa7c92c17abf2e3f6c1dddd4d682c48945ec8912
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-19 11:57:15 -0400

    Java Programming Day26

--- src/day26_statics/studentTask/StudentsGroup.java
+++ src/day26_statics/studentTask/StudentsGroup.java
@@ -8,7 +8,7 @@ public class StudentsGroup {
     public int groupId;
     public ArrayList<Student> students = new ArrayList<>();
 
-    public StudentsGroup(String groupName, int groupId) {
+    public StudentsGroup(String groupName, int groupId) { // creates the StudentsGroup object by setting the groupName and groupId
         this.groupName = groupName;
         this.groupId = groupId;
        // students = new ArrayList<>(); // size : 0
