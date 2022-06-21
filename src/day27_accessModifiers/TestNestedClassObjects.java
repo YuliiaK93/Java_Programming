0e6f8c90f0a08ce7688bc5415216122f3e2c7cee
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-21 15:03:56 -0400

    Java Programming Day27

--- src/day27_accessModifiers/TestNestedClassObjects.java
+++ src/day27_accessModifiers/TestNestedClassObjects.java
@@ -5,9 +5,9 @@ public class TestNestedClassObjects {
     public static void main(String[] args) {
 
         Car obj1 = new Car(); // Car object
-        obj1.new CarEngine(); // CarEngine object
+        Car.CarEngine obj2 = obj1.new CarEngine(); // CarEngine object
 
-       new Car.StaticInnerClass();
+        Car.StaticInnerClass obj3 =  new Car.StaticInnerClass();
 
 
 
