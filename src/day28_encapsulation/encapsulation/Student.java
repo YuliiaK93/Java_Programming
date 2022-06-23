95c1f984a594904075e67564222279692a3850a6
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-23 13:05:09 -0400

    Java Programming Day28

---
+++ src/day28_encapsulation/encapsulation/Student.java
@@ -0,0 +1,7 @@
+package day28_encapsulation.encapsulation;
+
+public class Student {
+
+    private int age;
+
+}
---
+++ src/day28_encapsulation/encapsulation/Test.java
@@ -0,0 +1,12 @@
+package day28_encapsulation.encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+       // student.age = -200;
+
+    }
+
+}
