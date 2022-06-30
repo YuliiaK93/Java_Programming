9b74eb94b6344e06f470b01418e1f828fe4194ca
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-30 14:31:47 -0400

    Java Programming Day31

--- src/day31_inheritance/animal_methodOverriding/Lion.java
+++ src/day31_inheritance/animal_methodOverriding/Lion.java
@@ -1,4 +1,19 @@
 package day31_inheritance.animal_methodOverriding;
 
-public class Lion {
+public class Lion extends Animal {
+
+    public Lion(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Lion "+getName()+" is eating deer");
+    }
+
+    public void roar(){
+        System.out.println("Lion "+getName()+" is roaring");
+    }
+
+
 }
