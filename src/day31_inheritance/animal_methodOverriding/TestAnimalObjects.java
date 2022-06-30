44b76a39f0adff4d0b26d40294c45c9484de8d27
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-06-30 14:52:03 -0400

    Java Programming Day31

---
+++ src/day31_inheritance/animal_methodOverriding/TestAnimalObjects.java
@@ -0,0 +1,19 @@
+package day31_inheritance.animal_methodOverriding;
+
+public class TestAnimalObjects {
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat("Emily", "Bangle", 'F', 4, "Small", "Gray");
+
+        Dog dog = new Dog("Max", "Husky", 'M', 3, "Large", "White");
+
+        Lion lion = new Lion("Simba", "African Lion", 'M', 6, "Large","Yellow", true);
+
+        Eagle eagle = new Eagle("Bella", "American Eagle", 'F', 4, "Medium", "Black & WHite");
+
+
+
+    }
+
+}
