7dcb032fea7097ab9f2a32718b70e7281c005887
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-08 16:14:44 -0400

    Java Programming Day35

--- src/day35_polymorphism/PolymorphismIntro.java
+++ src/day35_polymorphism/PolymorphismIntro.java
@@ -1,7 +1,14 @@
 package day35_polymorphism;
 
+import day31_inheritance.shape_methodOverriding.Circle;
+import day33_abstraction.employeeTask.*;
 import day34_abstraction.animalTask.*;
+import day34_abstraction.carTask.Audi;
+import day34_abstraction.carTask.Car;
+import day34_abstraction.carTask.Honda;
 import day34_abstraction.carTask.Tesla;
+
+import java.util.Arrays;
 
 public class PolymorphismIntro {
 
@@ -38,6 +45,54 @@ public class PolymorphismIntro {
 
       //  animal.play();
       //  animal.bark();
+
+
+        System.out.println("---------------------------------------------------");
+
+        String str = "Java";
+
+        Integer n = 100;
+
+        Boolean r = false;
+
+        Double d = 10.5;
+
+        Circle circle = new Circle(4);
+
+        Honda honda = new Honda("Pilot", "Black", 2019, 35000);
+
+        Audi audi = new Audi("Q6", "Silver", 2020, 36000);
+
+        Tesla tesla = new Tesla("Model Y", "Blue", 2022, 60000);
+
+        Teacher teacher = new Teacher("James", 45,  'M', "B1", "Math Teacher", 75000);
+
+        Developer developer = new Developer("Lucy", 30, 'F', "C1", "Java Developer", 95000, "Java");
+
+        Driver driver = new Driver("Aaron", 48, 'M', "D1", "Truck Driver", 90000);
+
+        Tester tester = new Tester("Emily", 35, 'F', "E1", "Manual Tester", 80000);
+
+
+        Object[] objects = {str, n, r, d, circle, honda, audi, tesla, teacher, developer, driver, tester};
+
+        System.out.println(Arrays.toString(objects));
+
+        Object[] employees = {teacher, developer, driver, tester};
+
+
+        Employee obj = new Driver("Aaron", 48, 'M', "D1", "Truck Driver", 90000);
+        obj.work();
+
+
+        System.out.println("------------------------------------------------------------");
+
+
+
+
+
+
+
 
 
 
