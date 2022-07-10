83f11a9b0acbe445b4686bc3eaee3dfb4276c9c9
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-10 12:50:37 -0400

    Java Programming Day36

--- src/day36_polymorphism/ReferenceTypeCastings.java
+++ src/day36_polymorphism/ReferenceTypeCastings.java
@@ -3,6 +3,9 @@ package day36_polymorphism;
 import day29_inheritance.animalTask.Animal;
 import day29_inheritance.animalTask.Cat;
 import day29_inheritance.animalTask.Dog;
+import day30_inheritance.phoneTask.IPhone;
+import day30_inheritance.phoneTask.Nokia;
+import day30_inheritance.phoneTask.Phone;
 
 public class ReferenceTypeCastings {
 
@@ -13,6 +16,39 @@ public class ReferenceTypeCastings {
       // Cat cat = new Dog();
 
         Animal animal = (Animal) new Dog(); // implicit casting
+
+        System.out.println("-----------------------------------------------------");
+
+        Animal animal1 = new Dog(); //upcasting ==> Polymorphism
+        animal1.setInfo("Max", "Husky", 'M', 4, "Large", "White");
+
+        animal1.eat();
+        animal1.drink();
+        animal1.sleep();
+
+        (  (Dog)animal1  ).bark();  // down casting
+
+      //  Dog dog1 = (Dog)animal1; // down casting
+     //   dog1.bark();
+
+      //  ( (Cat)animal1).scratch(); // Dog can not be converted to Cat, because there is not "IS A" relationship between dog and cat
+
+
+        System.out.println("-------------------------------------------------------------");
+
+        Phone phone = new Nokia("XR20", "Small", "Blue", 350);
+
+        phone.call(911);
+        phone.text(123456);
+        ((Nokia) phone).selfDefense();
+
+    //    ( (IPhone)phone ).faceTime(123456); // Nokia can not be converted to Iphone, because there is not "IS A" relationship between Nokia and Iphone
+
+        ((Nokia) phone).selfDefense();
+
+
+        System.out.println("-------------------------------------------------------------");
+
 
 
 
