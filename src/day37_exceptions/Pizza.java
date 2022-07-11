b083b4537107b9d5975faf72f39d5473b2f5e87e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-11 11:11:30 -0400

    Java Programming Day37

---
+++ src/day37_exceptions/Pizza.java
@@ -0,0 +1,58 @@
+package day37_exceptions;
+
+public class Pizza {
+
+    private char size;
+    private int numberOfCheeseTopping, numberOfPepperoniTopping;
+
+    public Pizza(char size, int numberOfCheeseTopping, int numberOfPepperoniTopping) {
+       setSize(size);
+       setNumberOfCheeseTopping(numberOfCheeseTopping);
+       setNumberOfPepperoniTopping(numberOfPepperoniTopping);
+    }
+
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    public int getNumberOfCheeseTopping() {
+        return numberOfCheeseTopping;
+    }
+
+    public void setNumberOfCheeseTopping(int numberOfCheeseTopping) {
+        this.numberOfCheeseTopping = numberOfCheeseTopping;
+    }
+
+    public int getNumberOfPepperoniTopping() {
+        return numberOfPepperoniTopping;
+    }
+
+    public void setNumberOfPepperoniTopping(int numberOfPepperoniTopping) {
+        this.numberOfPepperoniTopping = numberOfPepperoniTopping;
+    }
+}
+
+/*
+1. Create a class named Pizza
+			variables:
+				size (char), numberOfCheeseTopping, numberOfPepperoniTopping
+
+			Encapsulate all the fields
+
+			Add a constructor that can set all the fields
+
+			Methods:
+				calcCost(): returns the total cost of the pizza
+
+			Override toString method to print Pizza object info when the object is passed in the print statement
+
+			Override equals method that returns true if the pizza object is equal to the specified pizza object
+
+==
+
+ */
