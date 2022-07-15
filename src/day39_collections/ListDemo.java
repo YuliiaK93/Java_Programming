e2741953f81fd9c2edd22ebaf595ce1409359dc8
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-15 16:09:59 -0400

    Java Programming Day39

--- src/day39_collections/ListDemo.java
+++ src/day39_collections/ListDemo.java
@@ -2,6 +2,8 @@ package day39_collections;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Vector;
 
 
 public class ListDemo {
@@ -30,6 +32,34 @@ public class ListDemo {
 
 
         System.out.println("------------------------------------------------------------");
+
+
+        Vector<Integer> vector = new Vector<>();
+
+        vector.add(100);
+        vector.add(200);
+        vector.add(300);
+
+        vector.get(0);
+
+
+        System.out.println("------------------------------------------------------------");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(100);
+        stack.add(200);
+        stack.add(300);
+
+        System.out.println(stack);
+
+        int lastElement = stack.pop();
+
+        System.out.println(stack);
+
+        int secondLastElement = stack.pop();
+
+        System.out.println(secondLastElement);
+
 
 
 
