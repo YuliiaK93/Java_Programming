5293c7948cc2bddc8f2b06a432ffc3a9a9eee3a8
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-17 14:47:29 -0400

    Java Programming Day40

--- src/day40_collections/IterableExample.java
+++ src/day40_collections/IterableExample.java
@@ -2,6 +2,7 @@ package day40_collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class IterableExample {
@@ -9,7 +10,7 @@ public class IterableExample {
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
-        list.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3,4, 5, 6, 7, 1, 2, 3, 4, 5, 6,7));
+        list.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7));
 
         /*
         for (Integer each : list) {
@@ -21,13 +22,48 @@ public class IterableExample {
 
 
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i) < 5){
+            if (list.get(i) < 5) {
                 list.remove(i);
             }
         }
 
         System.out.println(list);
 
+
+        System.out.println("-----------------------------------------------------");
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7));
+
+        Iterator<Integer> it = list2.iterator();
+
+        while (it.hasNext()) {
+            Integer each = it.next();
+            if (each < 5) {
+                it.remove();
+            }
+        }
+
+        System.out.println(list2);
+
+
+        System.out.println("-----------------------------------------------------");
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7));
+
+        for (Iterator<Integer> i = list3.iterator(); i.hasNext();  ) {
+
+            Integer each = i.next();
+
+            if(each < 5){
+                i.remove();
+            }
+
+        }
+
+
+        System.out.println(list3);
 
 
 
