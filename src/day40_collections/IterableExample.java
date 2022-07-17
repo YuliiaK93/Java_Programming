cdccf38e432cd32c88ad9ee08f8962ff3d37902b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-17 14:56:29 -0400

    Java Programming Day40

--- src/day40_collections/IterableExample.java
+++ src/day40_collections/IterableExample.java
@@ -1,9 +1,6 @@
 package day40_collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class IterableExample {
 
@@ -76,6 +73,14 @@ public class IterableExample {
         System.out.println(list4);
 
 
+        System.out.println("-----------------------------------------------------");
+
+        List<Integer> list5 = new LinkedList<>();
+        list5.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7));
+
+        list5.removeIf( p -> p < 5);
+
+        System.out.println(list5);
 
     }
 
