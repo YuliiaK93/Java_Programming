d7ad9016ecd7dc45132313dc6a16c352c38b8a90
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-17 14:57:54 -0400

    Java Programming Day40

---
+++ src/day40_collections/IterablePractice1.java
@@ -0,0 +1,19 @@
+package day40_collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IterablePractice1 {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList(
+                "Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"
+        ));
+
+
+    }
+
+}
