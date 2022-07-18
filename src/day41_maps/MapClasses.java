f4e47a91ea42981f21c01fb33be1be13a44884ff
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-18 14:24:47 -0400

    Java Programming Day41

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -6,13 +6,16 @@ public class MapClasses {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> map1 = new HashMap<>(); // random order, accept null key
+        Map<String, Integer> map1 = new HashMap<>(); // random order, accept null key & null values
 
-        Map<String, Integer> map2 = new LinkedHashMap<>(); // insertion order, accepts null key
+        Map<String, Integer> map2 = new LinkedHashMap<>(); // insertion order, accepts null key  & null values
 
-        Map<String, Integer> map3 = new TreeMap<>(); // sorted order, does not accept null key
+        Map<String, Integer> map3 = new TreeMap<>(); // sorted order, does not accept null key, accepts null values
 
-        Map<String, Integer> map4 = new Hashtable<>(); // random order, does not accept null key, Synchronized
+        Map<String, Integer> map4 = new Hashtable<>(); // random order, does not accept null key & null values, Synchronized
+
+
+
 
 
 
