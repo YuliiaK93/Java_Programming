5f4c59811b7351532b18eb5068ba09744add5c2b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-07-19 14:53:57 -0400

    Java Programming Day42

--- src/day42_maps/MapPractice2.java
+++ src/day42_maps/MapPractice2.java
@@ -98,6 +98,25 @@ public class MapPractice2 {
 
 
 
+        System.out.println("---------------------------------------------------");
+
+        Map<String, Object> car1 = new LinkedHashMap<>();
+        car1.put("brand", "BMW");
+        car1.put("model", "X5");
+        car1.put("year", 2021);
+        car1.put("color", "Red");
+        car1.put("price", 45000.5);
+
+        Map<String, Object> car2 = new LinkedHashMap<>();
+
+        Map<String, Object> car3 = new LinkedHashMap<>();
+
+        Map<String, Object> car4 = new LinkedHashMap<>();
+
+
+        List<Map<String, Object>> cars = new ArrayList<>();
+        cars.addAll( Arrays.asList(car1, car2, car3, car4));
+
 
 
 
